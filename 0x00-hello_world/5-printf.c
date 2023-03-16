5b6315c0d90e2ae653b7a768bddc9a9ae0aaa024
marwakn <marwakandil13@gmail.com>
2023-03-16 16:24:37 -0700

    tache

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description:it's a c program that print text
+ * Description:it's a c program that printf text
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
