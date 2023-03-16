1da84ef232c571dbd77e3add8176035dceed2cea
marwakn <marwakandil13@gmail.com>
2023-03-16 16:00:44 -0700

    tache

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:it's a c program that execute a file
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+   gcc $CFILE -o cisfun
+   return (0);
+}
