694e9119d8cb02939ad10d62f404d402e2f346d7
marwakn <marwakandil13@gmail.com>
2023-03-16 17:37:00 -0700

    tache

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:it's a c program that execute a file
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("size of char is : ",sizeOf(char));
+printf("size of char is : ",sizeOf(int));
+printf("size of char is : ",sizeOf(long));
+printf("size of char is : ",sizeOf(float));
+return (0);
+}
