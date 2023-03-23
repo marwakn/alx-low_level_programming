62b576d69fe87b9676ee31246fdb7aa5a5f38dd5
marwakn <marwakandil13@gmail.com>
2023-03-23 08:41:46 -0700

    ghj

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -5,25 +5,10 @@
 * Return: 0
 */
 
-int main(void){
-        int positive_or_negative(int i){
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);	
-	};
-       
+int main(void)
+{
+ int i;
+        i = 0;
+        positive_or_negative(i);        
+	return (0);       
 }
