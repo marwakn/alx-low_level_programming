b93218268f92d73cd4c0940fc809d9ffdc4fdda1
marwakn <marwakandil13@gmail.com>
2023-05-05 17:27:24 -0700

    enfin valide inchallah

---
+++ 0x14-bit_manipulation/compile.sh
@@ -0,0 +1,7 @@
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main.c 1-print_binary.c _putchar.c -o b
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main.c 2-get_bit.c -o c
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main.c 3-set_bit.c -o d
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main.c 4-clear_bit.c -o e
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main.c 5-flip_bits.c -o f
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 main.c 100-get_endianness.c -o h
+
